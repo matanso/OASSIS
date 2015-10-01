f0abc70bd6d3d85fb914c9adb59dc7c854886762
matan <sokmatan@gmail.com>
2015-10-01 11:19:56 +0300

    Deploy

---
+++ Public/assets/js/queryController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by matan on 28/09/15.
+ */
+
+
+
+var app = angular.module('viewQueryApp', []);
+app.controller('queryController', ['$scope', '$sce', '$http', '$location', function ($scope, $sce, $http, $location) {
+    $scope.query = {};
+    var queryId = $location.search().queryId;
+    $http.get('app/query?queryId=' + queryId).then(function success(response){$scope.query=response.data}, function error(response){});
+}]);
