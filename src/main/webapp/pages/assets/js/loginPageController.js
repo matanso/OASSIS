c03745ee3f7d6d22e726c329de021c0912fe1448
matan <sokmatan@gmail.com>
2015-10-10 19:41:12 +0300

    Deploy

--- src/main/webapp/pages/assets/js/loginPageController.js
+++ src/main/webapp/pages/assets/js/loginPageController.js
@@ -10,33 +10,36 @@ app.controller('loginPageController', ['$scope', '$sce', '$http', function ($sco
     $scope.new = false;
 
     function login() {
-        $http({url: '/app/login',
-        method: "POST",
-        headers:  {'Content-Type': 'application/x-www-form-urlencoded'},
-                data: $.param({'loginType': 1, email: $scope.email, password: $scope.password})}
+        $http({
+                url: '/app/login',
+                method: "POST",
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: $.param({'loginType': 1, email: $scope.email, password: $scope.password})
+            }
         ).then(function success(response) {
-
+            console.log("success");
             },
             function error(response) {
+                console.log("error");
             });
     }
+
     function register() {
-        if($scope.password != $scope.password2)
-        {
+        if ($scope.password != $scope.password2) {
             $scope.error = "Passwords do not match.";
             return;
         }
-        $http({url: '/app/signup',
+        $http({
+                url: '/app/signup',
                 method: "POST",
-                headers:  {'Content-Type': 'application/x-www-form-urlencoded'},
-                data: $.param({'loginType': 1, name: $scope.name, email: $scope.email, password: $scope.password})}
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: $.param({'loginType': 1, name: $scope.name, email: $scope.email, password: $scope.password})
+            }
         ).then(function success(response) {
-                if(response.success == 0)
-                {
+                if (response.success == 0) {
                     // TODO
                 }
-                else
-                {
+                else {
                     $scope.error = response.error || "An error occurred.";
                 }
             },
@@ -44,6 +47,7 @@ app.controller('loginPageController', ['$scope', '$sce', '$http', function ($sco
 
             });
     }
+
     $scope.suggest = ["new user?", "login"];
     $scope.funcs = [login, register];
     $scope.texts = ["Login", "Sign up"];
