bf0a82e51a3751281eaf43f41b5e31b67f91accb
matan <sokmatan@gmail.com>
2015-10-12 16:49:37 +0300

    Deploy

--- src/main/webapp/pages/assets/js/logoutController.js
+++ src/main/webapp/pages/assets/js/logoutController.js
@@ -7,7 +7,7 @@
 var app = angular.module('logoutApp', []);
 app.controller('logoutController', ['$scope', '$sce', '$http', '$location', function ($scope, $sce, $http, $location) {
     $http.get('/app/logout').then(function success(response) {
-        location.window.href = 'index.html';
+        window.location.href = 'index.html';
     }, function error(response) {
     });
 }]);
