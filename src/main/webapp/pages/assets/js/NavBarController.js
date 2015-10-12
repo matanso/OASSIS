82175aa1467df072de8f5048ea465fa04e16e8c1
matan <sokmatan@gmail.com>
2015-10-12 20:54:49 +0300

    Deploy

--- src/main/webapp/pages/assets/js/NavBarController.js
+++ src/main/webapp/pages/assets/js/NavBarController.js
@@ -2,6 +2,7 @@
  * Created by matan on 12/10/15.
  */
 
+var app = angular.module('homepageApp');
 app.controller('NavBarController', ['$scope', '$sce', '$http', function ($scope, $sce, $http) {
     $scope.loggedIn = false;
     $http.get('/app/loggedIn').then(function success(response) {
