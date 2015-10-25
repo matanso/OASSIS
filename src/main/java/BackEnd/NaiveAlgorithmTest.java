d6cd1a81e5f513f62f63e61f70e3ba1b55dba061
dannyvainstein <dannyvainstein@gmail.com>
2015-10-25 09:41:00 +0200

    Update NaiveAlgorithmTest.java

--- src/main/java/BackEnd/NaiveAlgorithmTest.java
+++ src/main/java/BackEnd/NaiveAlgorithmTest.java
@@ -1,39 +1,39 @@
-/*
-package BackEnd;
+/***
+ * @author <a href="mailto:dannySarge@gmail.com">Danny V.</a>
+ * @version $Id: code-standards.xml,v 1.1 2015/10/24 taylor Exp $
+ */
 
-import java.io.File;
+package Oasis;
+
 import java.io.IOException;
-
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.rio.RDFParseException;
+/*import org.openrdf.sail.memory.MemoryStore;
+import com.google.gson.Gson;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.Repository;*/
 
-public class NaiveAlgorithmTest
+public class NaiveAlgorithmTest 
 {
+	
+	public static void main(String[] args) throws RepositoryException, RDFParseException, IOException, MalformedQueryException, QueryEvaluationException 
+	{
+		//note: TupleQueryResult has to have it's connection open in order to access.
+			
 
-    public static void main(String[] args) throws RepositoryException, RDFParseException, IOException, MalformedQueryException, QueryEvaluationException
-    {
-        //note: TupleQueryResult has to have it's connection open in order to access.
+		String queryString = "SELECT ?x ?y WHERE { ?x ?p ?y } ";
 
-
-        String queryString = "SELECT ?x ?y WHERE { ?x ?p ?y } ";
-		*/
-/*
-		Test 1: load 2 ontologies each containing one tuple. Then userID 1 gets a question, answers it to make it an MSP
-        then he asks for another question, gets the second tuple makes it an MSP as well, then asks for another question
-		and with none left gets null
-		*//*
-
-
-        String ontology4 = "C:\\Users\\vain\\danny2\\eclipse_workspace\\sesame-getting-started\\foursquare.ttl";
-        String ontology3 = "C:\\Users\\vain\\danny2\\eclipse_workspace\\sesame-getting-started\\threesquare.ttl";
-        File ontology4File = new File(ontology4);
-        File ontology3File = new File(ontology3);
+		/*Test 1: load 2 ontologies each containing one tuple. Then userID 1 gets a question, answers it to make it an MSP
+		then he asks for another question, gets the second tuple makes it an MSP as well, then asks for another question 
+		and with none left gets null*/
 		
-		*/
-/*SPARQLQueryManager.init(ontology3, "/Users/duanenickull/Software/neo4j-community-1.8.M01/");
+		String ontology3 = "C:\\Users\\vain\\danny2\\eclipse_workspace\\sesame-getting-started\\threesquare.ttl";
+
+		/*SPARQLQueryManager.init(ontology3, "/Users/duanenickull/Software/neo4j-community-1.8.M01/");
 		SPARQLQueryManager.loadOntology(ontology4File);
 		SPARQLQueryManager.loadOntology(ontology3File);
 		
@@ -45,55 +45,153 @@ public class NaiveAlgorithmTest
 		naiveAlgo.update(assignment, 1, 31);
 		assignment = naiveAlgo.next(1);
 
-		System.out.println("yay");*//*
-
+		System.out.println("yay");*/
 		
-		*/
-/*test 1: ontology has 2 entries. userID 1 asks for question makes it significant then asks for another question and makes that significant too.*//*
-
-
-        Wrapper.init("/Users/duanenickull/Software/neo4j-community-1.8.M01/", "Users/vain/danny2/CrowdSourcingTest2/");
-        Wrapper.loadOntology(ontology3);
-
-        Wrapper.submitQuery(queryString);
-
-        BindingSet bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
-        System.out.print("for user = " + 1 + " assignment = ");
-        SPARQLQueryManager.printBindingSet(bindingSet);
-        Wrapper.answerQuestion(bindingSet, queryString, 1, -1);
-
-
-        bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
-        System.out.print("for user = " + 1 + " assignment = ");
-        SPARQLQueryManager.printBindingSet(bindingSet);
-        Wrapper.answerQuestion(bindingSet, queryString, 1, 1);
-
-
-        bindingSet = (Wrapper.getQuestion(queryString, 2)).bindingSet;
-        System.out.print("for user = " + 2 + " assignment = ");
-        SPARQLQueryManager.printBindingSet(bindingSet);
-        Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
-
-
-        bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
-        System.out.print("for user = " + 1 + " assignment = ");
-        SPARQLQueryManager.printBindingSet(bindingSet);
-        Wrapper.answerQuestion(bindingSet, queryString, 1, 30);
-
-
-        bindingSet = (Wrapper.getQuestion(queryString, 2)).bindingSet;
-        System.out.print("for user = " + 2 + " assignment = ");
-        SPARQLQueryManager.printBindingSet(bindingSet);
-        Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
-
-        bindingSet = (Wrapper.getQuestion(queryString, 2)).bindingSet;
-        System.out.print("for user = " + 2 + " assignment = ");
-        SPARQLQueryManager.printBindingSet(bindingSet);
-        Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
-
-        System.out.println("yay");
-    }
-
-
+		/*test: users 1 and 2 make 2 assignemnts significant.*/
+		
+		/*Wrapper.init( "/Users/duanenickull/Software/neo4j-community-1.8.M01/", "Users/vain/danny2/CrowdSourcingTest2/");
+		Wrapper.loadOntology(ontology3);
+		
+		Wrapper.submitQuery(queryString, 30, 1);
+		
+		BindingSet bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, -1);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, 1);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, 30);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		System.out.println("yay");*/
+		
+		
+		
+		
+		/*test: userID 3 tries to get question after all assignments have been deemed significant by users 1 and 2.*/
+		
+		/*Wrapper.init( "/Users/duanenickull/Software/neo4j-community-1.8.M01/", "Users/vain/danny2/CrowdSourcingTest2/");
+		Wrapper.loadOntology(ontology3);
+		
+		Wrapper.submitQuery(queryString, 30, 1);
+		
+		BindingSet bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, -1);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, 1);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, 30);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  3)).bindingSet;
+		System.out.print("for user = " + 3 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 3, 30);*/
+		
+		
+		
+		/*test: given a random folder for both RDF and neo4j.*/
+		
+		/*Wrapper.init( "blasdfl", "blabalasdf");
+		Wrapper.loadOntology(ontology3);
+		
+		Wrapper.submitQuery(queryString, 30, 1);
+		
+		BindingSet bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, -1);*/
+		
+		/*test: negative values for support threshold and user number threshold - works like it's zero. */
+		
+		Wrapper.init( "/Users/duanenickull/Software/neo4j-community-1.8.M01/", "Users/vain/danny2/CrowdSourcingTest2/");
+		Wrapper.loadOntology(ontology3);
+		
+		Wrapper.submitQuery(queryString, -1, -1);
+		
+		BindingSet bindingSet = (Wrapper.getQuestion(queryString, 1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, -1);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, 1);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  1)).bindingSet;
+		System.out.print("for user = " + 1 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 1, 30);
+		
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+		
+		bindingSet = (Wrapper.getQuestion(queryString,  2)).bindingSet;
+		System.out.print("for user = " + 2 + " assignment = ");
+		SPARQLQueryManager.printBindingSet(bindingSet);
+		Wrapper.answerQuestion(bindingSet, queryString, 2, 30);
+	}
 }
-*/
